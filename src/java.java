03255b69e9b26b843ea13294e5bd3f846c488956
Xrr <zhuorongmo@outlook.com>
2022-08-16 14:02:03 +0800

    forth commit

--- src/java.java
+++ src/java.java
@@ -1,6 +1,10 @@
 public class java {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hello2world");
+        System.out.println("hello3world");
+        System.out.println("hello4world");
+        System.out.println("hello5world");
+        System.out.println("hello6world");
     }
 }
